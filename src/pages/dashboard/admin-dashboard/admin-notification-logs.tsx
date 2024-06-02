56ffee9f3d020daa341bc7ef9fed05a269bb44d2
John Ray <johnraycaete@gmail.com>
2024-06-02 10:58:11 +0800

    notification table added

---
+++ src/pages/dashboard/admin-dashboard/admin-notification-logs.tsx
@@ -0,0 +1,29 @@
+import NotificationOverview from "../../../components/admin-components/notification-overview";
+import Header from "../../../components/header";
+const AdminNotificationLogs = () => {
+  return (
+    <div
+      className={`relative flex flex-col w-full bg-fourth-light dark:bg-fourth-dark overflow-y-auto`}
+    >
+      <Header />
+      <div className=" w-full h-[200px] bg-primary-light p-4 ">
+        <p className="text-sixth-light font-semibold text-[25px]">
+          NOTIFICATION LOGS
+        </p>
+      </div>
+
+      <div className="flex-1 w-full px-4 absolute translate-y-32">
+        <div className="bg-white p-4 rounded-[10px] dark:bg-fourth-dark">
+          <div className="bg-sixth-light dark:bg-sixth-dark  shadow-lg rounded-[10px]">
+            <NotificationOverview />
+          </div>
+        </div>
+        <p className="px-4 mb-2 text-[14px] text-[#707070] text-center">
+          © Copyright reserve Friend Foundation Management System 2024
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AdminNotificationLogs
--- src/routes/protected.tsx
+++ src/routes/protected.tsx
@@ -5,6 +5,7 @@ import AdminUserList from "../pages/dashboard/admin-dashboard/admin-userlist";
 import AdminDistrictAssignment from "../pages/dashboard/admin-dashboard/admin-district-assignment";
 import AdminChurchAssignment from "../pages/dashboard/admin-dashboard/admin-church-assignment";
 import AdminAssignmentLogs from "../pages/dashboard/admin-dashboard/admin-assignment-logs";
+import AdminNotificationLogs from "../pages/dashboard/admin-dashboard/admin-notification-logs";
 
 const Protected = () => {
   return (
@@ -52,6 +53,16 @@ const Protected = () => {
             <div className="relative mx-auto w-full flex flex-row custom-scrollbar">
               <AdminSideBar />
               <AdminAssignmentLogs />
+            </div>
+          }
+        />
+
+        <Route
+          path="/dashboard/analytic-alert/notification"
+          element={
+            <div className="relative mx-auto w-full flex flex-row custom-scrollbar">
+              <AdminSideBar />
+              <AdminNotificationLogs />
             </div>
           }
         />
