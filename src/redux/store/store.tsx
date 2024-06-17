0cc0867e0bc2e686f2b69cc9c2ae6ea3a17ae340
John Ray <johnraycaete@gmail.com>
2024-06-17 19:50:28 +0800

    changes

--- src/redux/store/store.tsx
+++ src/redux/store/store.tsx
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { userApi } from "../services/usersApi";
 import { AuthenticationApi } from "../services/AuthenticationApi";
 import { ChurchApi } from "../services/ChurchApi";
 import { UserApi } from "../services/UserApi";
@@ -9,7 +8,6 @@ import { DistrictApi } from "../services/DistrictApi";
 import { AssignedLogsApi } from "../services/AssignedLogsApi";
 export const store = configureStore({
   reducer: {
-    [userApi.reducerPath]: userApi.reducer,
     [AuthenticationApi.reducerPath]: AuthenticationApi.reducer,
     [ChurchApi.reducerPath]: ChurchApi.reducer,
     [UserApi.reducerPath]: UserApi.reducer,
@@ -23,7 +21,6 @@ export const store = configureStore({
       ChurchApi.middleware,
       UserApi.middleware,
       AuthenticationApi.middleware,
-      userApi.middleware,
       StatsApi.middleware,
       DistrictApi.middleware,
       AssignedLogsApi.middleware
