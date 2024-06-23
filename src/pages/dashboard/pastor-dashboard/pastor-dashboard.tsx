add65aa60da54ce8cc29c2399e5118e9c8a054a7
John Ray <johnraycaete@gmail.com>
2024-06-23 13:46:35 +0800

    result cards added in pastor dashboard

--- src/pages/dashboard/pastor-dashboard/pastor-dashboard.tsx
+++ src/pages/dashboard/pastor-dashboard/pastor-dashboard.tsx
@@ -1,8 +1,12 @@
 import Header from "../../../components/header";
+import { FaFile } from "react-icons/fa6";
+import ResultCards from "../../../components/result-cards";
+import { useFetchNoOfFormsQuery, useFetchNoReviseFormQuery } from "../../../redux/services/StatsApi";
+import LoadingAnimation2 from "../../../components/loading-animation2";
 
 const PastorDashboard = () => {
-  // const { data: FormCount, isLoading: FormLoading } = useFetchNoOfFormsQuery();
-
+  const { data: FormCount, isLoading: FormLoading } = useFetchNoOfFormsQuery();
+  const {data: ReviseForm, isLoading: ReviseFormLoading} = useFetchNoReviseFormQuery()
   return (
     <div
       className={`relative flex flex-col w-full bg-fourth-light dark:bg-fourth-dark overflow-y-auto`}
@@ -14,13 +18,13 @@ const PastorDashboard = () => {
         </p>
       </div>
 
-      {/* <div className="flex flex-wrap flex-row gap-4 px-4 mt-[-4.5rem]">
+      <div className="flex flex-wrap flex-row gap-4 px-4 mt-[-4.5rem]">
         <ResultCards
           title="No. of Completed Forms"
           result={
             FormLoading ? <LoadingAnimation2 /> : FormCount?.completed_forms
           }
-          icon={HiMiniUsers}
+          icon={FaFile}
           navigation_path=""
         />
         <ResultCards
@@ -31,7 +35,15 @@ const PastorDashboard = () => {
           icon={FaFile}
           navigation_path=""
         />
-      </div> */}
+        <ResultCards
+          title="No. of Revise Forms"
+          result={
+            ReviseFormLoading ? <LoadingAnimation2 /> : ReviseForm?.count
+          }
+          icon={FaFile}
+          navigation_path=""
+        />
+      </div>
 
       <div className="w-full p-4 mt-4">
         <div className="bg-sixth-light dark:bg-sixth-dark shadow-lg rounded-[10px] custom-scrollbar"></div>
